9deb693252c47af4b670ba22e1e4387252572e1e
liuliangsir <2269305724@qq.com>
2017-08-04 23:58:09 +0800

    feat: 进一步完善操作cookie练习

--- get-set-cookie/test/cookie.spec.js
+++ get-set-cookie/test/cookie.spec.js
@@ -9,9 +9,9 @@ define(function (require) {
         describe('获取cookie成功', function () {
 
             it('key有效，cookie不为空', function () {
-                expect(cookieUtil.getCookie('cookie1', 'cookie1=testcookie; cookie2=yet another test')).toBe('testcookie');
-                expect(cookieUtil.getCookie('cookie2', 'cookie1=testcookie; cookie2=yet another test')).toBe('yet another test');
-                expect(cookieUtil.getCookie('cookie2', 'cookie1=testcookie;     cookie2=yet another test')).toBe('yet another test');
+                expect(cookieUtil.getCookie('cookie1', cookieUtil.cookie)).toBe('testcookie');
+                expect(cookieUtil.getCookie('cookie2', cookieUtil.cookie)).toBe('yet another test');
+                expect(cookieUtil.getCookie('cookie2', cookieUtil.cookie)).toBe('yet another test');
             });
 
         });
@@ -19,35 +19,35 @@ define(function (require) {
         describe('获取cookie失败', function () {
 
             it('key有效，cookie为空', function () {
-                expect(cookieUtil.getCookie('cookie1', '')).toBeNull();
+                expect(cookieUtil.getCookie('cookie1', cookieUtil.cookie)).toBeNull();
             });
             it('key无效，cookie为空', function () {
-                expect(cookieUtil.getCookie(' cookie1=', '')).toBeNull();
+                expect(cookieUtil.getCookie(' cookie1=', cookieUtil.cookie)).toBeNull();
             });
             it('key有效，cookie不为空', function () {
-                expect(cookieUtil.getCookie('cookie', 'cookie1=testcookie; cookie2=yet another test')).toBeNull();
+                expect(cookieUtil.getCookie('cookie', cookieUtil.cookie)).toBeNull();
             });
             it('key无效，cookie不为空', function () {
-                expect(cookieUtil.getCookie('; cookie', 'cookie1=testcookie; cookie2=yet another test')).toBeNull();
+                expect(cookieUtil.getCookie('; cookie', cookieUtil.cookie)).toBeNull();
             });
         });
 
         describe('设置cookie成功', function () {
 
             it('name, value存在', function () {
-                expect(cookieUtil.setCookie({key: 'cookie', value: 100})).toBe('cookie=100');
+                expect(cookieUtil.setCookie('cookie1', 100)).toBe('cookie=100');
             });
             it('name, value, expires存在', function () {
-                expect(cookieUtil.setCookie({key: 'cookie', value: 100, expires: 5})).toBe('cookie=100; expires=Fri, 04 Aug 2017 12:54:24 GMT');
+                expect(cookieUtil.setCookie('cookie2', 100, 5)).toBe('cookie=100; expires=Fri, 04 Aug 2017 12:54:24 GMT');
             });
             it('name, value, expires, domain存在', function () {
-                expect(cookieUtil.setCookie({key: 'cookie', value: 100, expires: 5, domain: 'abc.com'})).toBe('cookie=100; expires=Fri, 04 Aug 2017 12:54:24 GMT; domain=abc.com');
+                expect(cookieUtil.setCookie('cookie3', 100, 5, 'abc.com')).toBe('cookie=100; expires=Fri, 04 Aug 2017 12:54:24 GMT; domain=abc.com');
             });
             it('name, value, expires, domain, path存在', function () {
-                expect(cookieUtil.setCookie({key: 'cookie', value: 100, expires: 5, domain: 'abc.com', path: '/blog'})).toBe('cookie=100; expires=Fri, 04 Aug 2017 12:54:24 GMT; domain=abc.com; path=/blog');
+                expect(cookieUtil.setCookie('cookie4', 100, 5, 'abc.com', '/blog')).toBe('cookie=100; expires=Fri, 04 Aug 2017 12:54:24 GMT; domain=abc.com; path=/blog');
             });
             it('name, value, expires, domain, path, secure存在', function () {
-                expect(cookieUtil.setCookie({key: 'cookie', value: 100, expires: 5, domain: 'abc.com', path: '/blog', secure: true})).toBe('cookie=100; expires=Fri, 04 Aug 2017 12:54:24 GMT; domain=abc.com; path=/blog; secure');
+                expect(cookieUtil.setCookie('cookie5', 100, 5, 'abc.com', '/blog', true)).toBe('cookie=100; expires=Fri, 04 Aug 2017 12:54:24 GMT; domain=abc.com; path=/blog; secure');
             });
 
         });
@@ -55,7 +55,7 @@ define(function (require) {
         describe('设置cookie失败', function () {
 
             it('name, value, expires, domain, path, secure, HttpOnly存在', function () {
-                expect(cookieUtil.setCookie({key: 'cookie', value: 100, expires: 5, domain: 'abc.com', path: '/blog', secure: true, httpOnly: true})).toBe('cookie=100; expires=Fri, 04 Aug 2017 12:54:24 GMT; domain=abc.com; path=/blog; secure; HttpOnly');
+                expect(cookieUtil.setCookie('cookie6', 100, 5, 'abc.com', '/blog', true, true)).toBe('cookie=100; expires=Fri, 04 Aug 2017 12:54:24 GMT; domain=abc.com; path=/blog; secure; HttpOnly');
             });
 
         });
