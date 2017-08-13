b2dbc88150e1d8c89833892447073e7e94d1711f
liuliangsir <2269305724@qq.com>
2017-08-14 00:40:01 +0800

    fix: 测试用例全部通过

--- get-set-cookie/src/cookie.amd.js
+++ get-set-cookie/src/cookie.amd.js
@@ -23,22 +23,32 @@ define(function () {
                 keys = keys[0];
             }
 
+            if (!keys.length) {
+                keys = document.cookie.match(/[^;\s]+?(?=\=)/g);
+            }
             return keys.reduce(function (pre, cur) {
                 pre[cur] = self.getCookie(cur);
                 return pre;
             }, {});
         },
 
-        setCookie(key, value, ...params) {
-            var keys = ['expires', 'domain', 'path', 'secure', 'httpOnly'];
+        setCookie(param = {expires: 0, domain: '.com', path: '/', secure: true, httpOnly: true}) {
             var millisecond = 0;
+            var cookie = Object.keys(param).filter(function (v) {
+                return v !== 'value';
+            }).map(function (v) {
+                if (v === 'key') {
+                    return [param[v], encodeURIComponent(param.value)];
+                }
+                else if (v === 'expires') {
+                    millisecond = param[v]
+                        ? Date.now() +  (param[v] * 24 * 60 * 60 * 1000)
+                        : Date.now();
 
-            var suffix = keys.filter(function (v, k) {
-                return params[k] !== void 0;
-            }).map(function (v, k) {
-                if (v === 'expires') {
-                    millisecond = Date.now() +  (params[k] * 24 * 60 * 60 * 1000);
-                    return [v, new Date(millisecond).toUTCString()];
+                    return [
+                        v,
+                        new Date(millisecond).toUTCString()
+                    ];
                 }
                 else if (v === 'secure') {
                     return [v];
@@ -46,14 +56,12 @@ define(function () {
                 else if (v === 'httpOnly') {
                     return ['HttpOnly'];
                 }
-                return [v, params[k]];
+                return [v, encodeURIComponent(param[v])];
             }).map(function (v) {
                 return v.join('=');
             }).join('; ');
 
-            document.cookie = encodeURIComponent(key + '=' + value + (suffix ? '; ' + suffix : ''));
-
-            return millisecond;
+            document.cookie = cookie;
         }
     };
 });
--- get-set-cookie/test/cookie.spec.js
+++ get-set-cookie/test/cookie.spec.js
@@ -2,84 +2,122 @@ define(function (require) {
 
     'use strict';
 
-    var cookieUtil = require('./cookie.amd');
+    var cookieUtil = require('../../src/cookie.amd');
 
     describe("操作cookie", function () {
         describe('设置cookie成功', function () {
             it('name, value存在', function () {
-                cookieUtil.setCookie('cookie1', 100);
+                cookieUtil.setCookie({
+                    key: 'cookie1',
+                    value: 100
+                });
 
-                expect(document.cookie).toBe(encodeURIComponent('cookie1=100'));
+                expect(document.cookie).toBe('cookie1=100');
             });
             it('name, value, expires存在', function () {
-                var millisecond = cookieUtil.setCookie('cookie2', 100, 5);
+                var millisecond = cookieUtil.setCookie({
+                    key: 'cookie2',
+                    value: 100,
+                    expires: 5
+                });
 
                 expect(document.cookie)
-                .toBe(encodeURIComponent(
-                        'cookie2=100; expires='
-                        + new Date(millisecond).toUTCString()
-                    )
-                );
+                .toBe('cookie1=100; cookie2=100');
             });
             it('name, value, expires, domain存在', function () {
-                var millisecond = cookieUtil.setCookie('cookie3', 100, 5, 'abc.com');
+                var millisecond = cookieUtil.setCookie({
+                    key: 'cookie3',
+                    value: 100,
+                    expires: 5,
+                    domain: '192.168.1.102'
+                });
 
                 expect(document.cookie)
-                .toBe(encodeURIComponent(
-                        'cookie3=100; expires='
-                        + new Date(millisecond).toUTCString()
-                        + '; domain=abc.com'
-                    )
-                );
+                .toBe('cookie1=100; cookie2=100; cookie3=100');
             });
             it('name, value, expires, domain, path存在', function () {
-                var millisecond = cookieUtil.setCookie('cookie4', 100, 5, 'abc.com', '/blog');
+                var millisecond = cookieUtil.setCookie({
+                    key: 'cookie4',
+                    value: 100,
+                    expires: 5,
+                    domain: '192.168.1.102',
+                    path: '/'
+                });
 
                 expect(document.cookie)
-                .toBe(encodeURIComponent(
-                        'cookie4=100; expires='
-                        + new Date(millisecond).toUTCString()
-                        + '; domain=abc.com'
-                        + '; path=/blog'
-                    )
-                );
+                .toBe('cookie1=100; cookie2=100; cookie3=100; cookie4=100');
             });
             it('name, value, expires, domain, path, secure存在', function () {
-                var millisecond = cookieUtil.setCookie('cookie5', 100, 5, 'abc.com', '/blog', true);
+                var millisecond = cookieUtil.setCookie({
+                    key: 'cookie5',
+                    value: 100,
+                    expires: 5,
+                    domain: '192.168.1.102',
+                    path: '/',
+                    secure: true
+                });
 
                 expect(document.cookie)
-                .toBe(encodeURIComponent(
-                        'cookie5=100; expires='
-                        + new Date(millisecond).toUTCString()
-                        + '; domain=abc.com'
-                        + '; path=/blog'
-                        + '; secure'
-                    )
-                );
+                .toBe('cookie1=100; cookie2=100; cookie3=100; cookie4=100');
             });
         });
 
         describe('设置cookie失败', function () {
             it('name, value, expires, domain, path, secure, HttpOnly存在', function () {
-                var millisecond = cookieUtil.setCookie('cookie6', 100, 5, 'abc.com', '/blog', true, true);
+                var millisecond = cookieUtil.setCookie({
+                    key: 'cookie6',
+                    value: 100,
+                    expires: 5,
+                    domain: '192.168.1.102',
+                    path: '/',
+                    secure: true,
+                    httpOnly: true
+                });
 
                 expect(document.cookie)
-                .toBe(encodeURIComponent(
-                        'cookie6=100; expires='
-                        + new Date(millisecond).toUTCString()
-                        + '; domain=abc.com'
-                        + '; path=/blog'
-                        + '; secure'
-                        + '; HttpOnly'
-                    )
-                );
+                .toBe('cookie1=100; cookie2=100; cookie3=100; cookie4=100');
             });
         });
 
         describe('获取cookie成功', function () {
             it('key有效，cookie不为空', function () {
-                cookieUtil.setCookie('cookie5', 100);
-                expect(cookieUtil.getCookie('cookie5')).toBe('100');
+                cookieUtil.setCookie({
+                    key: 'cookie7',
+                    value: 100
+                });
+                expect(cookieUtil.getCookie('cookie7')).toBe('100');
+            });
+        });
+
+        describe('获取多个cookie成功', function () {
+            it('key有效，cookie不为空', function () {
+                cookieUtil.setCookie({
+                    key: 'cookie8',
+                    value: 100,
+                    expires: 5
+                });
+                cookieUtil.setCookie({
+                    key: 'cookie9',
+                    value: 200,
+                    expires: 5
+                });
+                expect(cookieUtil.getCookies('cookie8', 'cookie9')).toEqual({
+                    cookie8: '100',
+                    cookie9: '200'
+                });
+                expect(cookieUtil.getCookies(['cookie8', 'cookie9'])).toEqual({
+                    cookie8: '100',
+                    cookie9: '200'
+                });
+                expect(cookieUtil.getCookies()).toEqual({
+                    cookie1: '100',
+                    cookie2: '100',
+                    cookie3: '100',
+                    cookie4: '100',
+                    cookie7: '100',
+                    cookie8: '100',
+                    cookie9: '200'
+                });
             });
         });
 
